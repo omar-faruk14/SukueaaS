c4e907ed0f2e7c2e7602eacad10bc1a6aa5b434b
Omar Faruk <Omarfaruk885@gmail.com>
2024-10-28 17:31:27 +0900

    fixed issue for #ref:10282024

--- src/app/api/Admin/Event_Information/RecordID/[Record_number]/route.js
+++ src/app/api/Admin/Event_Information/RecordID/[Record_number]/route.js
@@ -102,7 +102,7 @@ export async function POST(request,{ params }) {
     };
     const formattedDate = formatDate(eventData.date);
     const formattedTime = formatTime(eventData.date);
-    const fullEventDetails = `📅 日時: ${formattedDate}\n⏰ 時間: ${formattedTime}-${eventData.End_Time}\n\n${eventData.Event_Line_Details}/n/nURL: ${eventData.Event_Link}`;
+    const fullEventDetails = `📅 日時: ${formattedDate}\n⏰ 時間: ${formattedTime}-${eventData.End_Time}\n\n${eventData.Event_Line_Details}\n\nURL: ${eventData.Event_Link}`;
     const imageUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/Admin/Event_Information/${eventData.Event_Image[0].fileKey}?width=240&height=160`;
 
     // Flex message template
