37349770444f397fa075d9799c1d915490b01d2d
Omar Faruk <Omarfaruk885@gmail.com>
2024-10-28 16:22:00 +0900

    Update #re:f10282024

--- src/app/api/Users/Moshikomi/route.js
+++ src/app/api/Users/Moshikomi/route.js
@@ -122,42 +122,42 @@ export async function POST(request) {
   }
 }
 
-const kintoneUrl = "https://emi-lab-osaka.cybozu.com/k/v1";
-const apiToken = "8x9GPZtE6VaTDjeMiY9SNWvjERFQy3wuv1OLxuC7";
-const appId = 48;
+// const kintoneUrl = "https://emi-lab-osaka.cybozu.com/k/v1";
+// const apiToken = "8x9GPZtE6VaTDjeMiY9SNWvjERFQy3wuv1OLxuC7";
+// const appId = 48;
 
-export async function GET(request) {
-  try {
-    const getRecordsResponse = await axios.get(
-      `${kintoneUrl}/records.json?app=${appId}`,
-      {
-        headers: {
-          "X-Cybozu-API-Token": apiToken,
-        },
-      }
-    );
+// export async function GET(request) {
+//   try {
+//     const getRecordsResponse = await axios.get(
+//       `${kintoneUrl}/records.json?app=${appId}`,
+//       {
+//         headers: {
+//           "X-Cybozu-API-Token": apiToken,
+//         },
+//       }
+//     );
 
-    const records = getRecordsResponse.data.records.map((record) => ({
-      id: record.$id.value,
-      Number_of_Participants: record.Number_of_Participants.value,
-      Additional_Comments: record.Additional_Comments.value,
-      Name: record.Name.value,
-      Line_User_ID: record.Line_User_ID.value,
-      Date_Time: record.Date_Time.value,
-      Location: record.Location.value,
-      Use_of_mobile_services: record.Use_of_mobile_services.value,
-      Boarding_and_alighting_place: record.Boarding_and_alighting_place.value,
-      Event_Name: record.Event_Name.value,
-      Desired_meeting_place: record.Desired_meeting_place.value,
-      Application_Type: record.Application_Type.value,
-      Preferred_Time: record.Preferred_Time.value,
-      Preferred_Date: record.Preferred_Date.value,
-      Destination: record.Destination.value,
-      Participant_Method: record.Participant_Method.value,
-    }));
-    return NextResponse.json(records);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json({ error: "Error retrieving records" });
-  }
-}
+//     const records = getRecordsResponse.data.records.map((record) => ({
+//       id: record.$id.value,
+//       Number_of_Participants: record.Number_of_Participants.value,
+//       Additional_Comments: record.Additional_Comments.value,
+//       Name: record.Name.value,
+//       Line_User_ID: record.Line_User_ID.value,
+//       Date_Time: record.Date_Time.value,
+//       Location: record.Location.value,
+//       Use_of_mobile_services: record.Use_of_mobile_services.value,
+//       Boarding_and_alighting_place: record.Boarding_and_alighting_place.value,
+//       Event_Name: record.Event_Name.value,
+//       Desired_meeting_place: record.Desired_meeting_place.value,
+//       Application_Type: record.Application_Type.value,
+//       Preferred_Time: record.Preferred_Time.value,
+//       Preferred_Date: record.Preferred_Date.value,
+//       Destination: record.Destination.value,
+//       Participant_Method: record.Participant_Method.value,
+//     }));
+//     return NextResponse.json(records);
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json({ error: "Error retrieving records" });
+//   }
+// }
